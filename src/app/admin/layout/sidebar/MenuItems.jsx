ee3119abe6681c4fa6e1c42d5ddd55b7f045b2da
gitking-han <hanzalarehman1000@gmail.com>
2025-11-29 02:52:57 -0800

    commit18

--- src/app/admin/layout/sidebar/MenuItems.jsx
+++ src/app/admin/layout/sidebar/MenuItems.jsx
@@ -64,42 +64,9 @@ const Menuitems = [
   { id: uniqueId(), title: "Login", icon: "login-2-broken", href: "/login" },
   { id: uniqueId(), title: "Register", icon: "shield-user-linear", href: "/signup" },
 
-  {
-    navlabel: true,
-    subheader: "OTHER",
-  },
-  {
-    id: uniqueId(),
-    title: "Menu Level",
-    icon: "double-alt-arrow-down-bold-duotone",
-    href: "https://spike-nextjs-pro-main.vercel.app/l1",
-    children: [
-      { id: uniqueId(), title: "Level 1", href: "https://spike-nextjs-pro-main.vercel.app/l1" },
-      {
-        id: uniqueId(),
-        title: "Level 1.1",
-        href: "https://spike-nextjs-pro-main.vercel.app/l1.1",
-        children: [
-          { id: uniqueId(), title: "Level 2", href: "https://spike-nextjs-pro-main.vercel.app/l2" },
-          {
-            id: uniqueId(),
-            title: "Level 2.1",
-            href: "https://spike-nextjs-pro-main.vercel.app/l2.1",
-            children: [
-              { id: uniqueId(), title: "Level 3", href: "https://spike-nextjs-pro-main.vercel.app/l3" },
-              { id: uniqueId(), title: "Level 3.1", href: "https://spike-nextjs-pro-main.vercel.app/l3.1" },
-            ],
-          },
-        ],
-      },
-    ],
-  },
+  
 
-  { id: uniqueId(), title: "Disabled", icon: "forbidden-circle-line-duotone", href: "", disabled: true },
-  { id: uniqueId(), title: "SubCaption", subtitle: "This is the sutitle", icon: "square-academic-cap-line-duotone", href: "https://spike-nextjs-pro-main.vercel.app/" },
-  { id: uniqueId(), title: "Chip", icon: "archive-check-line-duotone", href: "https://spike-nextjs-pro-main.vercel.app/", chipColor: "error" },
-  { id: uniqueId(), title: "Outlined", icon: "smile-circle-line-duotone", href: "https://spike-nextjs-pro-main.vercel.app/" },
-  { id: uniqueId(), title: "External Link", external: true, icon: "link-bold-duotone", href: "https://google.com" },
+  
 ];
 
 export default Menuitems;
